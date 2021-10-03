d3b105b9517b64ab9c3e544530995f16ad187a45
aki hayashi <hayashi1019toy@gmail.com>
2021-10-03 14:19:44 +0900

    set default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kenkoutecho_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = { host: 'safe-ocean-41138.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'safe-ocean-41138.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {

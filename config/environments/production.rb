27bede5e9c9f3cd4ecf1c02423a5fcb1507a3efc
aki hayashi <hayashi1019toy@gmail.com>
2021-10-03 13:48:14 +0900

    change SMTP from sendgrid to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,16 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kenkoutecho_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'safe-ocean-41138.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'safe-ocean-41138.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'safe-ocean-41138.herokuapp.com',
+    :authentication => :plain,
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
